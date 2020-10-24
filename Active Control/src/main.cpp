2a348c7ea561b99c8711d19d2e38c68f3ffdcf31
mht3 <mht3@illinois.edu>
2020-10-24 12:54:17 -0500

    added data thread

--- Active Control/src/main.cpp
+++ Active Control/src/main.cpp
@@ -6,21 +6,49 @@
 
 #define SERVO1_PIN 5
 #define SERVO2_PIN 6
+#define SERVO3_PIN 7
+#define SERVO4_PIN 8
+#define TERMINATE 9900000000000
+
 Servo servo1;
 Servo servo2;
+Servo servo3;
+Servo servo4;
+
 int initialAngle = 0;
 int currentAngle;
 int altitude; 
 
 //Gives thread 32 bytes. STATIC, so size will not change.
 static THD_WORKING_AREA(waThread, 32);
+//May have to change the size
+static THD_WORKING_AREA(waThread2, 32);
+
+static THD_FUNCTION(dataThread, arg) {
+    (void)arg;
+    // start the SPI library:
+    SPI.begin();
+    /*
+    //Need IMU, Pitot, and GPS data from Beaglebone...
+    
+    Datasheet for IMU: https://www.mouser.com/datasheet/2/389/lsm9ds1-1849526.pdf
+    Datasheet for Pressure Sensor: https://www.te.com/commerce/DocumentDelivery/DDEController?Action=srchrtrv&DocNm=MS5611-01BA03&DocType=Data+Sheet&DocLang=English
+    Datasheet for Accelerometer: https://www.mouser.com/datasheet/2/348/KX134-1211-Specifications-Rev-1.0-1659717.pdf
+    Datasheet for GPS: https://www.u-blox.com/sites/default/files/ZOE-M8_DataSheet_%28UBX-16008094%29.pdf
+
+    Initalize the data and chip select pins:
+
+    */
+   
+}
 
 static THD_FUNCTION(servoThread, arg) {
     (void)arg;
     //while loop is in place of loop() at the bototom
     while(true) {
-        if(servo1.attached() == false || servo2.attached() == false) {
-            delay(9900000000000); //basically end the program
+        if(servo1.attached() == false || servo2.attached() == false
+        || servo3.attached() == false || servo4.attached() == false) {
+            delay(TERMINATE); //basically end the program
         }
         // constrain limits range of angle values to between a and b, and does x if x is inbetween this range.
         //Not sure if we will need constrain yet (maybe if we do roll).
@@ -29,10 +57,7 @@ static THD_FUNCTION(servoThread, arg) {
         //servo2.write(constrain(theta,a,b)); 
     }
 }
-
-void chSetup() {
-    servo1.attach(SERVO1_PIN);
-    servo2.attach(SERVO2_PIN);
+void testServos() {
     currentAngle = initialAngle;
     //For loops test the functionality of the servo's movements;
     for (currentAngle; currentAngle <= initialAngle + 90; currentAngle++) {
@@ -44,8 +69,26 @@ void chSetup() {
         servo2.write(currentAngle);
     }
 
+    //Testing second set of servos
+    for (currentAngle; currentAngle <= initialAngle + 90; currentAngle++) {
+        servo3.write(currentAngle);
+        servo4.write(currentAngle);
+    }
+    for (currentAngle; currentAngle >= initialAngle; currentAngle--) {
+        servo3.write(currentAngle);
+        servo4.write(currentAngle);
+    }
+}
+
+void chSetup() {
+    servo1.attach(SERVO1_PIN);
+    servo2.attach(SERVO2_PIN);
+    servo3.attach(SERVO3_PIN);
+    servo4.attach(SERVO4_PIN);
+    testServos();
     //Start servoThread
     chThdCreateStatic(waThread, sizeof(waThread), NORMALPRIO, servoThread, NULL);
+    chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, dataThread,NULL);
     while(true) {
         //Spawning Thread. We just need to keep it running in order to no lose servoThread
         chThdSleep(100);
@@ -58,7 +101,6 @@ void setup() {
     while(true) {}
     //Must stay in while true loop to keep the chSetup thread running
     }
-
 
 void loop() {
     //Could cause problems if used for ChibiOS, 
