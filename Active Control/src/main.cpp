ddf3d6b84208403e4fba636e328b9605a533fa30
mht3 <mht3@illinois.edu>
2020-11-02 21:33:49 -0600

    Added logging thread via serial for data as well as an extras file with other random thoughts

--- Active Control/src/main.cpp
+++ Active Control/src/main.cpp
@@ -3,26 +3,31 @@
 #include <ChRt.h>
 #include <Servo.h>
 #include <SPI.h>
-#include <SD.h>
 
 
-#define SERVO1_PIN 5
-#define SERVO2_PIN 6
-#define SERVO3_PIN 8
+#define SERVO1_PIN 3
+#define SERVO2_PIN 5
+#define SERVO3_PIN 6
 #define SERVO4_PIN 9
-#define TERMINATE 9900000000
+const unsigned int TERMINATE = 99000000;
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
-File logFile;
 
-const int chip = BUILTIN_SDCARD;
 
-int initialAngle = 0;
-int currentAngle;
-int altitude; 
+int currentAngle; //Current angle of the servos. 
+int initialAngle = 0; //Initial angle of servos. This may not be zero.
+float velocity; //current velocity of the rocket
+float az; //Acceleration in the z direction
+float altitude; //current altitude from altimiter
+float roll_rate; //angular velocity in the z direction
+float g = 9.81; //Acceleration due to gravity in m/s
+float lattitude; //current lattitude from gps
+float longitude; //current longitude from gps
+
+
 
 //Gives thread 32 bytes. STATIC, so size will not change.
 static THD_WORKING_AREA(waThread, 32);
@@ -31,6 +36,8 @@ static THD_WORKING_AREA(waThread2, 32);
 //Workign area thread for logger
 static THD_WORKING_AREA(waThread3, 32);
 
+struct logMessage{
+};
 
 //Thread to get data from sensors
 static THD_FUNCTION(dataThread, arg) {
@@ -46,71 +53,36 @@ static THD_FUNCTION(dataThread, arg) {
     Datasheet for GPS: https://www.u-blox.com/sites/default/files/ZOE-M8_DataSheet_%28UBX-16008094%29.pdf
 
     Initalize the data and chip select pins:
-
+    The values will be filled soon
     */
+   velocity = 0;
+   az = 0;
+   altitude = 0;
+   roll_rate = 0; 
+   lattitude = 0;
+   longitude = 0;
    
 }
 //Thread to log information to BeagleBone. This will be done after data is read.
-//https://www.pjrc.com/store/sd_adaptor.html
 static THD_FUNCTION(loggerThread, arg) {
     (void)arg;
-    Serial.begin(9600);
-    while (!Serial) {
-        //Wait to connect to serial
+    Serial.begin(115200);
+    float sensorData[6] = {altitude, az, lattitude, longitude, roll_rate, velocity};
+    byte *data_asByteArray = (byte*)sensorData;
+    for(byte i = 0; i < sizeof(sensorData); i++){
+        Serial.write(data_asByteArray[i]);
+        //Serial.write(*(data_asByteArray+i));
     }
-    Serial.print("Initializing SD card...");
-    if (!SD.begin(chip)) {
-        Serial.println("initialization failed!");
-        return;
-    }
-    Serial.println("initialization done.");
-    
-    // open the file. 
-    logFile = SD.open("AC_Logger.txt", FILE_WRITE);
-    
-    // if the file opened okay, write to it:
-    if (logFile) {
-        Serial.print("Writing to AC_Logger.txt...");
-        logFile.println("Log test: (Can you read this");
-        // close the file:
-        logFile.close();
-        Serial.println("done.");
-    } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening AC_Logger.txt");
-    }
-    
-    // re-open the file for reading:
-    logFile = SD.open("AC_Logger.txt");
-    if (logFile) {
-        Serial.println("AC_Logger.txt:");
-        
-        // read from the file until there's nothing else in it:
-        while (logFile.available()) {
-            Serial.write(logFile.read());
-        }
-        // close the file:
-        logFile.close();
-    } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening AC_Logger.txt");
-    }
+
 }
-
-
+//Thread for actuating servos for active control
 static THD_FUNCTION(servoThread, arg) {
     (void)arg;
     //while loop is in place of loop() at the bototom
     while(true) {
-        if(servo1.attached() == false || servo2.attached() == false
-        || servo3.attached() == false || servo4.attached() == false) {
+        if(servo1.attached() == false || servo2.attached() == false || servo3.attached() == false || servo4.attached() == false) {
             delay(TERMINATE); //basically end the program
         }
-        // constrain limits range of angle values to between a and b, and does x if x is inbetween this range.
-        //Not sure if we will need constrain yet (maybe if we do roll).
-        //float theta = 90;
-        //servo1.write(constrain(theta,a,b));
-        //servo2.write(constrain(theta,a,b)); 
     }
 }
 void testServos() {
@@ -164,5 +136,3 @@ void loop() {
     //Could cause problems if used for ChibiOS, 
     //but needed to make teensy happy.
 }
-
-
