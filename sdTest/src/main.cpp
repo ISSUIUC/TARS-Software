ab9f18ebd9a96a2256992db7f42200d77b854874
AyberkY <ayberkyaraneri@gmail.com>
2020-12-12 19:57:46 -0600

    data logging works on T4.1

--- sdTest/src/main.cpp
+++ sdTest/src/main.cpp
@@ -1,5 +1,6 @@
-#include <Arduino.h>
-#include <ChRt.h>
+
+#include "ChRt.h"
+
 #include<SPI.h>
 #include<SD.h>
 
@@ -28,24 +29,19 @@ int timeStamp;
 File dataFile;
 char fileName[12];
 
-static THD_WORKING_AREA(dataThread_WA, 32);
+static THD_WORKING_AREA(dataThread_WA, 256);
 thread_t *dataThread_Pointer;
 
 static THD_FUNCTION(dataThread, arg) {
     (void)arg;
-    /*
-    Need IMU, Pitot, and GPS data from Beaglebone...
 
-    Datasheet for IMU: https://www.mouser.com/datasheet/2/389/lsm9ds1-1849526.pdf
-    Datasheet for Pressure Sensor: https://www.te.com/commerce/DocumentDelivery/DDEController?Action=srchrtrv&DocNm=MS5611-01BA03&DocType=Data+Sheet&DocLang=English
-    Datasheet for Accelerometer: https://www.mouser.com/datasheet/2/348/KX134-1211-Specifications-Rev-1.0-1659717.pdf
-    Datasheet for GPS: https://www.u-blox.com/sites/default/files/ZOE-M8_DataSheet_%28UBX-16008094%29.pdf
-
-    Initalize the data and chip select pins:
-    The values will be filled soon
-    */
     while (true) {
-        
+
+        Serial.println("yayeet");
+        digitalWrite(LED_BUILTIN, HIGH);
+        chThdSleepMilliseconds(50);
+        digitalWrite(LED_BUILTIN, LOW);
+
         velocity = 0;
         az = 0;
         altitude = 0;
@@ -57,72 +53,54 @@ static THD_FUNCTION(dataThread, arg) {
         PT3 = 0;
         timeStamp = 0;
 
-        //string to hold data to write all at once.
-        char dataStr[100] = "";
-        char buffer[8];
-
-        dtostrf(velocity,6,6,buffer); //convert float to char[]
-        strcat(dataStr,buffer); //cat char[] to end of dataStr
-        strcat(dataStr,","); //add comma to seperate values
-
-        dtostrf(az,6,6,buffer);
-        strcat(dataStr,buffer);
-        strcat(dataStr,",");
-
-        dtostrf(altitude,6,6,buffer);
-        strcat(dataStr,buffer);
-        strcat(dataStr,",");
-
-        dtostrf(roll_rate,6,6,buffer);
-        strcat(dataStr,buffer);
-        strcat(dataStr,",");
-
-        dtostrf(latitude,6,6,buffer);
-        strcat(dataStr,buffer);
-        strcat(dataStr,",");
-
-        dtostrf(longitude,6,6,buffer);
-        strcat(dataStr,buffer);
-        strcat(dataStr,",");
-
-        dtostrf(PT1,6,6,buffer);
-        strcat(dataStr,buffer);
-        strcat(dataStr,",");
-
-        dtostrf(PT2,6,6,buffer);
-        strcat(dataStr,buffer);
-        strcat(dataStr,",");
-
-        dtostrf(PT3,6,6,buffer);
-        strcat(dataStr,buffer);
+        dataFile.print(velocity);
+        dataFile.print(",");
+        dataFile.print(az);
+        dataFile.print(",");
+        dataFile.print(altitude);
+        dataFile.print(",");
+        dataFile.print(roll_rate);
+        dataFile.print(",");
+        dataFile.print(latitude);
+        dataFile.print(",");
+        dataFile.print(longitude);
+        dataFile.print(",");
+        dataFile.print(PT1);
+        dataFile.print(",");
+        dataFile.print(PT2);
+        dataFile.print(",");
+        dataFile.print(PT3);
+        dataFile.print(",");
+        dataFile.print(timeStamp);
+        dataFile.print(",");
 
         //Writing line of data to SD card
-        dataFile = SD.open(fileName, FILE_WRITE);
-        dataFile.println(dataStr);
-        dataFile.close();
+        dataFile.println("ehhlo");
+        dataFile.flush();
     }
 }
 
 void chSetup() {
-    
-    dataThread_Pointer = chThdCreateStatic(dataThread_WA, sizeof(dataThread_WA), NORMALPRIO, dataThread,NULL);
 
-    while(true) {
-        //Spawning Thread. We just need to keep it running in order to no lose servoThread
-        chThdSleep(100);
-    }
+    dataThread_Pointer = chThdCreateStatic(dataThread_WA, sizeof(dataThread_WA),
+                                           NORMALPRIO, dataThread, NULL);
+
 }
 
 
 
 void setup() {
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
     // start the SPI library:
     SPI.begin();
     Serial.begin(9600);
+    while (!Serial) {}
 
     //SD Card Setup
     if(!SD.begin(BUILTIN_SDCARD)){
-        //this is what executes if Teensy fails to communicate with SD card. That is not good. Probably no-go for launch
+        Serial.println("SD Begin Failed");
     }
 
     strcpy(fileName,"data.csv");
@@ -159,21 +137,23 @@ void setup() {
 
     Serial.println("opening SD");
     Serial.println(fileName);
-    //write header for csv file
-    dataFile=SD.open(fileName);
+
+    dataFile = SD.open(fileName, O_CREAT | O_WRITE | O_TRUNC);
     Serial.println("SD opened");
+
     dataFile.println("velocity,z acceleration,altitude,roll rate,latitude,longitude,PT1,PT2,PT3");
     Serial.println("wrote header");
-    dataFile.close();
-    Serial.println("closed file");
 
     Serial.println("starting chibiOS");
     //Initialize and start ChibiOS (Technically the first thread)
     chBegin(chSetup);
+
     while(true) {}
     //Must stay in while true loop to keep the chSetup thread running
 }
 
 void loop() {
+    chThdSleepMilliseconds(1000);
+
   //not used
 }
