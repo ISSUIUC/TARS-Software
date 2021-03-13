aba8d1f86684abc6ffc59887eb78afbebeee68fb
mht3 <60635839+mht3@users.noreply.github.com>
2021-03-13 13:53:33 -0600

    changed gps to .hpp

--- testFlight1/src/main.cpp
+++ testFlight1/src/main.cpp
@@ -7,7 +7,7 @@
 
 #include "SparkFunLSM9DS1.h" //Low-G IMU Library
 #include "KX134-1211.h" //High-G IMU Library
-#include "ZOEM8Q0.h" //GPS Library
+#include "ZOEM8Q0.hpp" //GPS Library
 #include "hybridShared.h"
 #include "acShared.h"
 #include "dataLog.h"
