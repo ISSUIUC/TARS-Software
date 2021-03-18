8bac1efceb24b91884844581e4b54caf1d738af9
Joshua Blustein <blusteinjoshua@gmail.com>
2021-03-17 20:37:14 -0500

    seperated data struct definitions

--- testFlight1/lib/teensyShared/dataLog.h
+++ testFlight1/lib/teensyShared/dataLog.h
@@ -5,35 +5,7 @@
 #include <SD.h>
 
 #include "acShared.h"
-
-struct dataStruct_t {
-    float ax;
-    float ay;
-    float az;
-    float gx;
-    float gy;
-    float gz;
-    float mx;
-    float my;
-    float mz;
-
-    //! data for highGimu accel data (hg_x, hg_y, hg_z) 
-    float hg_ax;
-    float hg_ay;
-    float hg_az;
-    // int16_t pt1;
-    // int16_t pt2;
-    // int16_t pt3;
-
-    //GPS DATA
-    float latitude;
-    float longitude;
-    float altitude;
-    bool posLock;
-    
-    int32_t timeStamp;
-
-};
+#include "dataStructs.h"
 
 void init_dataLog(File* dataFile);
 
