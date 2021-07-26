646a3d7b099245f6a799d1defa381f7cff91c526
AyberkY <ayberkyaraneri@gmail.com>
2021-07-25 23:32:46 -0500

    Now using GoogleTest for unit tests!!

---
+++ flight/mcu/lib/DummyTestClass/DummyTestClass.cpp
@@ -0,0 +1,13 @@
+/**
+ * Stupid simple test class to mess with GoogleTest/GMock
+ */
+
+#include "DummyTestClass.h"
+
+int DummyTestClass::getValue() {
+		return value;
+}
+
+void DummyTestClass::setValue(int val) {
+		value = val;
+}
---
+++ flight/mcu/test/target/dummy.cpp
@@ -0,0 +1,7 @@
+
+// Nothing to see here
+
+
+int main(int argc, char **argv) {
+  return 0;
+}
