1bf3c86af14fa5e596f55c5be2fb57f3b0fc1183
NPIPHI <nwp2@illinois.edu>
2021-09-27 18:37:33 -0500

    clang format

--- flight/mcu/lib/teensyShared/fifo.h
+++ flight/mcu/lib/teensyShared/fifo.h
@@ -5,17 +5,17 @@
 #ifndef TARS_SOFTWARE_FIFO_H
 #define TARS_SOFTWARE_FIFO_H
 
-#include<cstdint>
-#include<cstring>
-#include<mutex>
+#include <cstdint>
+#include <cstring>
+#include <mutex>
 
 // only works with types that can be copied with memcpy
 // switch to whatever mutex works on the platform
-class FifoBuffer{
+class FifoBuffer {
    public:
     // buff_size = size of array in elements
     // data_size = size of array element in bytes
-    FifoBuffer(void * arr, uint16_t buff_size, uint16_t data_size){
+    FifoBuffer(void* arr, uint16_t buff_size, uint16_t data_size) {
         capacity_ = buff_size;
         cur_length_ = 0;
         head_idx_ = 0;
@@ -24,14 +24,14 @@ class FifoBuffer{
         arr_ = arr;
     }
 
-    bool push(void * element){
+    bool push(void* element) {
         std::lock_guard<std::mutex> l(lock);
-        if(capacity_ == cur_length_){
+        if (capacity_ == cur_length_) {
             return false;
         }
 
         size_t byte_offset = data_size_ * tail_idx_;
-        void * tail_ptr = (void*)((char*)arr_ + byte_offset);
+        void* tail_ptr = (void*)((char*)arr_ + byte_offset);
 
         std::memcpy(tail_ptr, element, data_size_);
 
@@ -42,15 +42,15 @@ class FifoBuffer{
         return true;
     }
 
-    //returns false on failure
-    bool pop(void * out){
+    // returns false on failure
+    bool pop(void* out) {
         std::lock_guard<std::mutex> l(lock);
-        if(cur_length_ == 0){
+        if (cur_length_ == 0) {
             return false;
         }
 
         size_t byte_offset = data_size_ * head_idx_;
-        void * head_ptr = (void*)((char*)arr_ + byte_offset);
+        void* head_ptr = (void*)((char*)arr_ + byte_offset);
         std::memcpy(out, head_ptr, data_size_);
 
         head_idx_ += 1;
@@ -62,7 +62,7 @@ class FifoBuffer{
 
    private:
     uint16_t capacity_, cur_length_, head_idx_, tail_idx_, data_size_;
-    void * arr_;
+    void* arr_;
     std::mutex lock;
 };
 
