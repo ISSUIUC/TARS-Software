77f8a598b23261549d3455f9aaa6e4a4847b0b83
NPIPHI <nwp2@illinois.edu>
2021-10-10 19:48:46 -0500

    removed dummy matrix

--- flight/mcu/lib/teensyShared/ActiveControl.h
+++ flight/mcu/lib/teensyShared/ActiveControl.h
@@ -1,5 +1,5 @@
-#include <../eigen-3.4.0/Eigen/Core>
-#include <../eigen-3.4.0/Eigen/Dense>
+#include "../eigen-3.4.0/Eigen/Core"
+#include "../eigen-3.4.0/Eigen/Dense"
 
 using namespace Eigen;
 
@@ -17,8 +17,4 @@ class ActiveControl {
             {0.005},
             {-0.005}
         };
-
-        //These matrices are for future full-state estimation purposes. Keep here for stress testing?
-        Matrix<float, 16, 16> k; 
-
 };
