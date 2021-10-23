7318776d3fd8f4ca2ab4257564fb305ecd37e935
Nicholas Phillips <nwp2@illinois.edu>
2021-10-22 21:45:21 -0500

    typed perf tests

--- flight/mcu/test/test_target/test_FifoBuffer/test_FifoBuffer_perf.cpp
+++ flight/mcu/test/test_target/test_FifoBuffer/test_FifoBuffer_perf.cpp
@@ -63,6 +63,93 @@ static THD_FUNCTION(consumer_perf_thread, arg) {
 }
 
 void test_producer_performance_perf(){
+    chThdCreateStatic(producerPerfThreadWorkingArea1,
+                sizeof(producerPerfThreadWorkingArea1),
+                NORMALPRIO,  /* Initial priority.    */
+                producer_perf_thread,    /* Thread function.     */
+                NULL);       /* Thread parameter.    */
+    chThdCreateStatic(consumerPerfThreadWorkingArea1,
+                sizeof(consumerPerfThreadWorkingArea1),
+                NORMALPRIO,  /* Initial priority.    */
+                consumer_perf_thread,    /* Thread function.     */
+                NULL);       /* Thread parameter.    */
+
+    //let the two threads start so thread startup time does not impact perf testing
+    chThdSleep(100);
+
+
+    chSemSignal(&start_producer_perf_thread);
+    chSemSignal(&start_comsumer_perf_thread);
+    auto begin = chVTGetSystemTime();
+
+    
+    chSemWait(&done_producer_perf_thread);
+    chSemWait(&done_consumer_perf_thread);
+    auto end = chVTGetSystemTime();
+    auto tick_time = 1.f / CH_CFG_ST_FREQUENCY;
+    Serial.printf("PERF: 10000 pushes in %f s\n", (end - begin) * tick_time);
+    TEST_ASSERT_LESS_THAN(100, (end - begin) * tick_time * 1000);
+}
+
+/******************************************************************************/
+/* TYPED PRODUCER-CONSUMER PERF THREAD TEST                                              */
+
+FifoBuffer<int, 10> typed_thread_perf_fifo{};
+
+static THD_FUNCTION(typed_producer_perf_thread, arg) {
+
+    chSemWait(&start_producer_perf_thread);
+
+    int i = 0;
+
+    for(i=0;i<=10000;i++){
+        while (!typed_thread_perf_fifo.push(i)){
+            //let the other thread get the lock if the buffer is full
+            chThdSleep(1);
+        };
+    }
+    
+
+    chSemSignal(&done_producer_perf_thread);
+}
+
+
+static THD_FUNCTION(typed_consumer_perf_thread, arg) {
+
+    chSemWait(&start_comsumer_perf_thread);
+
+    int i,j;
+
+    for(j=0;j<=10000;j++){
+
+        while(!typed_thread_perf_fifo.pop(&i)){
+            //let the other thread get the lock if the buffer is empty
+            chThdSleep(1);
+        };
+        
+        TEST_ASSERT_EQUAL(i, j);
+    }
+
+    chSemSignal(&done_consumer_perf_thread);
+}
+
+void typed_test_producer_performance_perf(){
+    chThdCreateStatic(producerPerfThreadWorkingArea1,
+                sizeof(producerPerfThreadWorkingArea1),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_producer_perf_thread,    /* Thread function.     */
+                NULL);       /* Thread parameter.    */
+    chThdCreateStatic(consumerPerfThreadWorkingArea1,
+                sizeof(consumerPerfThreadWorkingArea1),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_consumer_perf_thread,    /* Thread function.     */
+                NULL);       /* Thread parameter.    */
+
+    //let the two threads start so thread startup time does not impact perf testing
+    chThdSleep(100);
+
+
+
     chSemSignal(&start_producer_perf_thread);
     chSemSignal(&start_comsumer_perf_thread);
     auto begin = chVTGetSystemTime();
@@ -201,26 +288,120 @@ void test_many_producer_perf(){
     TEST_ASSERT_LESS_THAN(100, (end - begin) * tick_time * 1000);
 }
  
+ 
+/******************************************************************************/
+/* TYPED MANY PRODUCER ONE CONSUMER PERF THREAD TEST */    
+
+FifoBuffer<int, 10> typed_prod_1_fifo{};
+FifoBuffer<int, 10> typed_prod_2_fifo{};
+FifoBuffer<int, 10> typed_prod_3_fifo{};
+FifoBuffer<int, 10> typed_prod_4_fifo{};
+FifoBuffer<int, 10> typed_prod_5_fifo{};
+
+static THD_FUNCTION(typed_many_producer_thread, arg) {
+    auto buffer = (FifoBuffer<int,10>*)arg;
+    chSemWait(&producer_start);
+
+    for(int i=0;i< 10000;i++){
+        while (!buffer->push(i)){
+            //let the other thread get the lock if the buffer is full
+            chThdSleep(1);
+        };
+    }
+    
+
+    chSemSignal(&producer_end);
+}
+
+static THD_FUNCTION(typed_main_consumer_thread, arg) {
+
+    chSemWait(&main_consumer_start);
+
+    for(int i = 0; i < 10000; i++){
+        int v1,v2,v3,v4,v5;
+        while(!typed_prod_1_fifo.pop(&v1)){chThdSleep(1);};
+        while(!typed_prod_2_fifo.pop(&v2)){chThdSleep(1);};
+        while(!typed_prod_3_fifo.pop(&v3)){chThdSleep(1);};
+        while(!typed_prod_4_fifo.pop(&v4)){chThdSleep(1);};
+        while(!typed_prod_5_fifo.pop(&v5)){chThdSleep(1);};
+        
+        TEST_ASSERT_EQUAL(i, v1);
+        TEST_ASSERT_EQUAL(i, v2);
+        TEST_ASSERT_EQUAL(i, v3);
+        TEST_ASSERT_EQUAL(i, v4);
+        TEST_ASSERT_EQUAL(i, v5);
+    }
+
+    chSemSignal(&main_consumer_end);
+}
+
+
+void typed_test_many_producer_perf(){
+    chThdCreateStatic(producer1_WA,
+                sizeof(producer1_WA),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_many_producer_thread,    /* Thread function.     */
+                &typed_prod_1_fifo);       /* Thread parameter.    */
+    chThdCreateStatic(producer2_WA,
+                sizeof(producer2_WA),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_many_producer_thread,    /* Thread function.     */
+                &typed_prod_2_fifo);       /* Thread parameter.    */
+    chThdCreateStatic(producer3_WA,
+                sizeof(producer3_WA),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_many_producer_thread,    /* Thread function.     */
+                &typed_prod_3_fifo);       /* Thread parameter.    */
+    chThdCreateStatic(producer4_WA,
+                sizeof(producer4_WA),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_many_producer_thread,    /* Thread function.     */
+                &typed_prod_4_fifo);       /* Thread parameter.    */
+    chThdCreateStatic(producer5_WA,
+                sizeof(producer5_WA),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_many_producer_thread,    /* Thread function.     */
+                &typed_prod_5_fifo);       /* Thread parameter.    */
+    chThdCreateStatic(main_consumer_WA,
+                sizeof(main_consumer_WA),
+                NORMALPRIO,  /* Initial priority.    */
+                typed_main_consumer_thread,    /* Thread function.     */
+                NULL);       /* Thread parameter.    */
+
+    //wait for threads to start
+    chThdSleep(100);
+    
+    chSemSignal(&main_consumer_start);
+    chSemSignal(&producer_start);
+    chSemSignal(&producer_start);
+    chSemSignal(&producer_start);
+    chSemSignal(&producer_start);
+    chSemSignal(&producer_start);
+    auto begin = chVTGetSystemTime();
+
+    
+    chSemWait(&producer_end);
+    chSemWait(&producer_end);
+    chSemWait(&producer_end);
+    chSemWait(&producer_end);
+    chSemWait(&producer_end);
+    chSemWait(&main_consumer_end);
+    auto end = chVTGetSystemTime();
+    auto tick_time = 1.f / CH_CFG_ST_FREQUENCY;
+    Serial.printf("PERF: 10000 pushes in %f s\n", (end - begin) * tick_time);
+    TEST_ASSERT_LESS_THAN(100, (end - begin) * tick_time * 1000);
+}
+ 
 
 /******************************************************************************/
 /* RUN THREADED TEST CASES                                                    */
 
 void run_performance_test_cases(){
-    chThdCreateStatic(producerPerfThreadWorkingArea1,
-                sizeof(producerPerfThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                producer_perf_thread,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
-    chThdCreateStatic(consumerPerfThreadWorkingArea1,
-                sizeof(consumerPerfThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                consumer_perf_thread,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
-
-    //let the two threads start so thread startup time does not impact perf testing
-    chThdSleep(100);
-
+    
     RUN_TEST(test_producer_performance_perf);
+    RUN_TEST(typed_test_producer_performance_perf);
     RUN_TEST(test_many_producer_perf);
+    RUN_TEST(typed_test_many_producer_perf);
+
 }
 
