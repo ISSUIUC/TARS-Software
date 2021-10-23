97a5f7c3b76905ce0a5bc6485f2ed3e26363e00d
Nicholas Phillips <nwp2@illinois.edu>
2021-10-22 22:12:40 -0500

    clang format

--- flight/mcu/test/test_target/test_FifoBuffer/test_FifoBuffer_threads.cpp
+++ flight/mcu/test/test_target/test_FifoBuffer/test_FifoBuffer_threads.cpp
@@ -46,16 +46,14 @@ static THD_FUNCTION(typed_myThread, arg) {
     chSemSignal(&typed_thread_done);
 }
 
-void typed_test_thread_push(){
+void typed_test_thread_push() {
     chSemSignal(&typed_start_thread);
     chSemWait(&typed_thread_done);
-
 
     int i;
     TEST_ASSERT(typed_thread_fifo.pop(&i));
     TEST_ASSERT_EQUAL(i, 5);
 }
-
 
 /******************************************************************************/
 /* PRODUCER-CONSUMER THREAD TEST TYPED                                        */
@@ -69,18 +67,16 @@ SEMAPHORE_DECL(typed_done_consumerThread1, 0);
 FifoBuffer<int, 10> typed_thread_fifo2{};
 
 static THD_FUNCTION(typed_producerThread1, arg) {
-
     chSemWait(&typed_start_producerThread1);
 
     int i = 0;
 
-    for(i=0;i<=100;i++){
-        while (!typed_thread_fifo2.push(i)){
-            //let the other thread get the lock if the buffer is full
+    for (i = 0; i <= 100; i++) {
+        while (!typed_thread_fifo2.push(i)) {
+            // let the other thread get the lock if the buffer is full
             chThdSleep(1);
         }
     }
-    
 
     chSemSignal(&typed_done_producerThread1);
 }
@@ -88,21 +84,21 @@ static THD_FUNCTION(typed_producerThread1, arg) {
 static THD_FUNCTION(typed_consumerThread1, arg) {
     chSemWait(&typed_start_consumerThread1);
 
-    int i,j;
+    int i, j;
 
-    for(j=0;j<=100;j++){
-        while(!typed_thread_fifo2.pop(&i)){
-            //let the other thread get the lock if the buffer is empty
+    for (j = 0; j <= 100; j++) {
+        while (!typed_thread_fifo2.pop(&i)) {
+            // let the other thread get the lock if the buffer is empty
             chThdSleep(1);
         }
-        
+
         TEST_ASSERT_EQUAL(i, j);
     }
 
     chSemSignal(&typed_done_consumerThread1);
 }
 
-void typed_test_producer_consumer(){
+void typed_test_producer_consumer() {
     chSemSignal(&typed_start_consumerThread1);
     chSemSignal(&typed_start_producerThread1);
 
@@ -122,25 +118,24 @@ SEMAPHORE_DECL(typed_done_pushThread1, 0);
 FifoBuffer<int, 10> typed_thread_fifo3{};
 
 static THD_FUNCTION(typed_pushThread1, arg) {
-
     chSemWait(&typed_start_pushThread1);
 
     int i = 0;
 
-    while (!typed_thread_fifo3.push(i));
+    while (!typed_thread_fifo3.push(i))
+        ;
 
     chSemSignal(&typed_done_pushThread1);
 }
 
 static THD_FUNCTION(typed_popThread1, arg) {
-
     chSemWait(&typed_start_popThread1);
 
     int i;
 
     // Push the first time and it should work.
     TEST_ASSERT_TRUE(typed_thread_fifo3.pop(&i));
-        
+
     TEST_ASSERT_EQUAL(i, 0);
 
     // Push the second time and it should fail
@@ -149,28 +144,26 @@ static THD_FUNCTION(typed_popThread1, arg) {
     chSemSignal(&typed_done_popThread1);
 }
 
-void typed_test_thread_pop_fail(){
+void typed_test_thread_pop_fail() {
     chSemSignal(&typed_start_pushThread1);
     chSemWait(&typed_done_pushThread1);
 
     chSemSignal(&typed_start_popThread1);
     chSemWait(&typed_done_popThread1);
-
 }
 
 /******************************************************************************/
 /* THREAD PUSH FAILING TEST TYPED                                             */
 
-SEMAPHORE_DECL(typed_start_pushThread2,0);
-SEMAPHORE_DECL(typed_done_pushThread2,0);
+SEMAPHORE_DECL(typed_start_pushThread2, 0);
+SEMAPHORE_DECL(typed_done_pushThread2, 0);
 
-SEMAPHORE_DECL(typed_start_pushThread3,0);
-SEMAPHORE_DECL(typed_done_pushThread3,0);
+SEMAPHORE_DECL(typed_start_pushThread3, 0);
+SEMAPHORE_DECL(typed_done_pushThread3, 0);
 
 FifoBuffer<int, 1> typed_thread_fifo4{};
 
-
-static THD_FUNCTION(typed_pushThread2, arg){
+static THD_FUNCTION(typed_pushThread2, arg) {
     chSemWait(&typed_start_pushThread2);
 
     int i = 1;
@@ -179,7 +172,7 @@ static THD_FUNCTION(typed_pushThread2, arg){
     chSemSignal(&typed_done_pushThread2);
 }
 
-static THD_FUNCTION(typed_pushThread3, arg){
+static THD_FUNCTION(typed_pushThread3, arg) {
     chSemWait(&typed_start_pushThread3);
 
     int i = 2;
@@ -187,13 +180,12 @@ static THD_FUNCTION(typed_pushThread3, arg){
     TEST_ASSERT_FALSE(typed_thread_fifo4.push(i));
 
     // After failing it should still returning the pushed number
-    TEST_ASSERT_EQUAL(1,typed_thread_fifo4.pop(&i));
+    TEST_ASSERT_EQUAL(1, typed_thread_fifo4.pop(&i));
 
     chSemSignal(&typed_done_pushThread3);
-
 }
 
-void typed_test_thread_push_fail(){
+void typed_test_thread_push_fail() {
     chSemSignal(&typed_start_pushThread2);
     chSemWait(&typed_done_pushThread2);
 
@@ -326,16 +318,16 @@ void test_thread_pop_fail() {
 /******************************************************************************/
 /* THREAD PUSH FAILING TEST                                                   */
 
-SEMAPHORE_DECL(start_pushThread2,0);
-SEMAPHORE_DECL(done_pushThread2,0);
+SEMAPHORE_DECL(start_pushThread2, 0);
+SEMAPHORE_DECL(done_pushThread2, 0);
 
-SEMAPHORE_DECL(start_pushThread3,0);
-SEMAPHORE_DECL(done_pushThread3,0);
+SEMAPHORE_DECL(start_pushThread3, 0);
+SEMAPHORE_DECL(done_pushThread3, 0);
 
 int thread_data4[1];
 GenericFifoBuffer thread_fifo4(thread_data4, 1, sizeof(int));
 
-static THD_FUNCTION(pushThread2, arg){
+static THD_FUNCTION(pushThread2, arg) {
     chSemWait(&start_pushThread2);
 
     int i = 1;
@@ -344,7 +336,7 @@ static THD_FUNCTION(pushThread2, arg){
     chSemSignal(&done_pushThread2);
 }
 
-static THD_FUNCTION(pushThread3, arg){
+static THD_FUNCTION(pushThread3, arg) {
     chSemWait(&start_pushThread3);
 
     int i = 2;
@@ -352,20 +344,18 @@ static THD_FUNCTION(pushThread3, arg){
     TEST_ASSERT_FALSE(thread_fifo4.push(&i));
 
     // After failing it should still returning the pushed number
-    TEST_ASSERT_EQUAL(1,thread_fifo4.pop(&i));
+    TEST_ASSERT_EQUAL(1, thread_fifo4.pop(&i));
 
     chSemSignal(&done_pushThread3);
-
 }
 
-void test_thread_push_fail(){
+void test_thread_push_fail() {
     chSemSignal(&start_pushThread2);
     chSemWait(&done_pushThread2);
 
     chSemSignal(&start_pushThread3);
     chSemWait(&done_pushThread3);
 }
-
 
 /******************************************************************************/
 /* RUN THREADED TEST CASES                                                    */
@@ -382,84 +372,77 @@ void run_threaded_test_cases() {
                       producerThread1, /* Thread function.     */
                       NULL);           /* Thread parameter.    */
     chThdCreateStatic(consumerThreadWorkingArea1,
-                sizeof(consumerThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                consumerThread1,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+                      sizeof(consumerThreadWorkingArea1),
+                      NORMALPRIO,      /* Initial priority.    */
+                      consumerThread1, /* Thread function.     */
+                      NULL);           /* Thread parameter.    */
 
-    chThdCreateStatic(popThreadWorkingArea1,
-                sizeof(popThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                popThread1,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
-    chThdCreateStatic(pushThreadWorkingArea1,
-                sizeof(pushThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                pushThread1,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+    chThdCreateStatic(popThreadWorkingArea1, sizeof(popThreadWorkingArea1),
+                      NORMALPRIO, /* Initial priority.    */
+                      popThread1, /* Thread function.     */
+                      NULL);      /* Thread parameter.    */
+    chThdCreateStatic(pushThreadWorkingArea1, sizeof(pushThreadWorkingArea1),
+                      NORMALPRIO,  /* Initial priority.    */
+                      pushThread1, /* Thread function.     */
+                      NULL);       /* Thread parameter.    */
 
-    chThdCreateStatic(pushThreadWorkingArea2,
-                sizeof(pushThreadWorkingArea2),
-                NORMALPRIO,  /* Initial priority.    */
-                pushThread2,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
-    chThdCreateStatic(pushThreadWorkingArea3,
-                sizeof(pushThreadWorkingArea3),
-                NORMALPRIO,  /* Initial priority.    */
-                pushThread3,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+    chThdCreateStatic(pushThreadWorkingArea2, sizeof(pushThreadWorkingArea2),
+                      NORMALPRIO,  /* Initial priority.    */
+                      pushThread2, /* Thread function.     */
+                      NULL);       /* Thread parameter.    */
+    chThdCreateStatic(pushThreadWorkingArea3, sizeof(pushThreadWorkingArea3),
+                      NORMALPRIO,  /* Initial priority.    */
+                      pushThread3, /* Thread function.     */
+                      NULL);       /* Thread parameter.    */
 
-
-    // New tests, typed 
+    // New tests, typed
     RUN_TEST(test_thread_push);
     RUN_TEST(test_producer_consumer);
     RUN_TEST(test_thread_pop_fail);
     RUN_TEST(test_thread_push_fail);
 
     chThdCreateStatic(typed_myThreadWorkingArea,
-                sizeof(typed_myThreadWorkingArea),
-                NORMALPRIO,  /* Initial priority.    */
-                typed_myThread,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+                      sizeof(typed_myThreadWorkingArea),
+                      NORMALPRIO,     /* Initial priority.    */
+                      typed_myThread, /* Thread function.     */
+                      NULL);          /* Thread parameter.    */
 
     chThdCreateStatic(typed_producerThreadWorkingArea1,
-                sizeof(typed_producerThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                typed_producerThread1,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+                      sizeof(typed_producerThreadWorkingArea1),
+                      NORMALPRIO,            /* Initial priority.    */
+                      typed_producerThread1, /* Thread function.     */
+                      NULL);                 /* Thread parameter.    */
     chThdCreateStatic(typed_consumerThreadWorkingArea1,
-                sizeof(typed_consumerThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                typed_consumerThread1,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+                      sizeof(typed_consumerThreadWorkingArea1),
+                      NORMALPRIO,            /* Initial priority.    */
+                      typed_consumerThread1, /* Thread function.     */
+                      NULL);                 /* Thread parameter.    */
 
     chThdCreateStatic(typed_popThreadWorkingArea1,
-                sizeof(typed_popThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                typed_popThread1,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+                      sizeof(typed_popThreadWorkingArea1),
+                      NORMALPRIO,       /* Initial priority.    */
+                      typed_popThread1, /* Thread function.     */
+                      NULL);            /* Thread parameter.    */
     chThdCreateStatic(typed_pushThreadWorkingArea1,
-                sizeof(typed_pushThreadWorkingArea1),
-                NORMALPRIO,  /* Initial priority.    */
-                typed_pushThread1,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+                      sizeof(typed_pushThreadWorkingArea1),
+                      NORMALPRIO,        /* Initial priority.    */
+                      typed_pushThread1, /* Thread function.     */
+                      NULL);             /* Thread parameter.    */
 
     chThdCreateStatic(typed_pushThreadWorkingArea2,
-                sizeof(typed_pushThreadWorkingArea2),
-                NORMALPRIO,  /* Initial priority.    */
-                typed_pushThread2,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
+                      sizeof(typed_pushThreadWorkingArea2),
+                      NORMALPRIO,        /* Initial priority.    */
+                      typed_pushThread2, /* Thread function.     */
+                      NULL);             /* Thread parameter.    */
     chThdCreateStatic(typed_pushThreadWorkingArea3,
-                sizeof(typed_pushThreadWorkingArea3),
-                NORMALPRIO,  /* Initial priority.    */
-                typed_pushThread3,    /* Thread function.     */
-                NULL);       /* Thread parameter.    */
-
+                      sizeof(typed_pushThreadWorkingArea3),
+                      NORMALPRIO,        /* Initial priority.    */
+                      typed_pushThread3, /* Thread function.     */
+                      NULL);             /* Thread parameter.    */
 
     // New tests
     RUN_TEST(typed_test_thread_push);
     RUN_TEST(typed_test_producer_consumer);
     RUN_TEST(typed_test_thread_pop_fail);
     RUN_TEST(typed_test_thread_push_fail);
-
 }
