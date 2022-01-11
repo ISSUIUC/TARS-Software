fc9724dc8af3d231ab485219121bcf97b40e9b85
Nicholas Phillips <nwp2@illinois.edu>
2022-01-11 12:43:08 -0600

    got serial working

--- ground/groundGUI/src/index.ts
+++ ground/groundGUI/src/index.ts
@@ -1,17 +1,32 @@
 import { app, BrowserWindow } from 'electron';
+import * as SerialPort from "serialport"
 import * as path from 'path';
 
 app.on('ready', () => {
     console.log('App is ready');
-
     const win = new BrowserWindow({
         width: 600,
         height: 400,
-        webPreferences: {nodeIntegration: true}
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            contextIsolation: false,
+        }
     }); 
 
     const indexHTML = path.join(__dirname + '/index.html');
-    win.loadFile(indexHTML, ).then(() => {
-        // IMPLEMENT FANCY STUFF HERE
+    win.loadFile(indexHTML).then(() => {
+        serial_communicate(win);
     }).catch(e => console.error(e));
 });
+
+
+function serial_communicate(window: BrowserWindow){
+    setInterval(async ()=>{
+        const ports = await SerialPort.list();
+        window.webContents.send("serial", JSON.stringify(ports));
+    }, 1000);
+    // const port = new SerialPort("COM3");
+    // const msg = port.read(128);
+    // window.webContents.send("serial", msg);
+}
--- ground/groundGUI/src/main.ts
+++ ground/groundGUI/src/main.ts
@@ -1,17 +1,17 @@
-import * as SerialPort from "serialport";
+import { ipcRenderer } from "electron";
 
 const led_status = document.getElementById("led_status");
 const led_button = document.getElementById("Blink");
 led_status.innerHTML = "0";
 document.body.appendChild(led_status);
 
-let port = new SerialPort("idk");
-
 let led = false;
 
-led_button.addEventListener("click", ()=>{
-    led = !led;
-    const text = led ? "ON" : "OFF";
-    led_status.innerHTML = text;
-    port.write(text);
+// led_button.addEventListener("click", ()=>{
+ipcRenderer.on("serial",(event, message)=>{
+    led_status.innerHTML = message;
+    // led = !led;
+    // const text = led ? "ON" : "OFF";
+    // led_status.innerHTML = text;
+    // // port.write(text);
 });
