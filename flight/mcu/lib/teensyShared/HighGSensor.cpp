e36023ba7173d9781d69e35adb928b2e8a8eb88f
Aryaman Dwivedi <aryaman.dwivedi@hotmail.com>
2022-02-13 14:32:53 -0600

    added a highgsenor file

--- flight/mcu/lib/teensyShared/HighGSensor.cpp
+++ flight/mcu/lib/teensyShared/HighGSensor.cpp
@@ -1,5 +1,17 @@
+#include <KX134-1211.h>
+#include <ChRt.h>
 #include "HighGSensor.h"
 
-void HighGSensor::readReadings(){
-    
+void HighGSensor::readReadings() {
+    chSysLock();
+    KX->update_data();
+    chSysUnlock();
+}
+
+GForce HighGSensor::getGForce() {
+    float x_gforce=KX->get_x_gforce();
+    float y_gforce=KX->get_y_gforce();
+    float z_gforce=KX->get_z_gforce();
+
+    return GForce{x_gforce,y_gforce,z_gforce};
 }
--- flight/mcu/lib/teensyShared/HighGSensor.h
+++ flight/mcu/lib/teensyShared/HighGSensor.h
@@ -1,1 +1,18 @@
+#ifndef HIGHGSENSOR_H
+#define HIGHGSENSOR_H
+
 #include <KX134-1211.h>
+
+struct HighGSensor {
+    KX134* KX;
+    void readReadings();
+    GForce getGForce();
+};
+
+struct GForce {
+    float x_gforce;
+    float y_gforce;
+    float z_gforce;
+};
+
+#endif
