2634e33bd1903ccaa0f599cfb1f544c67d548006
Aryaman Dwivedi <aryaman.dwivedi@hotmail.com>
2022-02-14 18:06:47 -0600

    GPS Sensor rewritten

---
+++ flight/mcu/lib/teensyShared/GPSSensor.h
@@ -0,0 +1,24 @@
+#ifndef GPSSENSOR_H
+#define GPSSENSOR_H
+
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+
+struct GPSSensor {
+    SFE_UBLOX_GNSS* GNSS;
+    void readReadings();
+    systime_t timeStamp_GPS;
+    float latitude;
+    float longtitude;
+    float altitude;
+    uint32_t fix_type;
+    bool pos_lock;
+    uint32_t SIV_count;
+    float getLatitude();
+    float getLongtitude();
+    float getAltitude();
+    uint32_t getFixType();
+    bool getPosLock();
+    uint32_t getSIVCount();
+};
+
+#endif
