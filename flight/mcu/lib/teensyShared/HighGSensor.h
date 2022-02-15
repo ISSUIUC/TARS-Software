393cf884b3d1b1537c967699c07df17fc011de98
Aryaman Dwivedi <aryaman.dwivedi@hotmail.com>
2022-02-14 18:16:09 -0600

    Bug fix for struct defintion

--- flight/mcu/lib/teensyShared/HighGSensor.h
+++ flight/mcu/lib/teensyShared/HighGSensor.h
@@ -3,16 +3,18 @@
 
 #include <KX134-1211.h>
 
-struct HighGSensor {
-    KX134* KX;
-    void readReadings();
-    GForce getGForce();
-};
 
 struct GForce {
     float x_gforce;
     float y_gforce;
     float z_gforce;
 };
+struct HighGSensor {
+    KX134* KX;
+    void readReadings();
+    GForce getGForce();
+};
+
+
 
 #endif
--- flight/mcu/lib/teensyShared/LowGSensor.h
+++ flight/mcu/lib/teensyShared/LowGSensor.h
@@ -3,13 +3,6 @@
 
 #include <SparkFunLSM9DS1.h>
 
-struct LowGSensor {
-    LSM9DS1* LSM;
-    void readReadings();
-    Acceleration getAcceleration();
-    Gyroscope getGyroscope();
-    Magnetometer getMagnetometer();
-};
 
 struct Acceleration {
     float ax;
@@ -28,6 +21,15 @@ struct Magnetometer {
     float my;
     float mz;
 };
+struct LowGSensor {
+    LSM9DS1* LSM;
+    void readReadings();
+    Acceleration getAcceleration();
+    Gyroscope getGyroscope();
+    Magnetometer getMagnetometer();
+};
+
+
 
 
 
