6bf8fc175dbf7096e8e878d36b4d635399f55a11
Gautam Dayal <gautam.dayal@yahoo.com>
2022-06-29 13:01:25 -0500

    Removed old TODO comment

--- flight/mcu/lib/teensyShared/thresholds.h
+++ flight/mcu/lib/teensyShared/thresholds.h
@@ -2,20 +2,25 @@
 #ifndef THRESHOLDS_H
 #define THRESHOLDS_H
 
-// TODO PUT BACK
-
-// transition from boost to coast if low accleration detected for 300 ms
+// Transition from boost to coast if low accleration detected for 300 ms
 static const float coast_time_thresh = 300;
+
 // 2 gs for launch detect
 static const float launch_linear_acceleration_thresh = 2;
-// required time of detected acceleration to switch to boost
+
+// Required time of detected acceleration to switch to boost
 static const float launch_time_thresh = 300;
-// if we observe less than 0.5 g of accel, transition to coast
+
+// If we observe less than 0.5 g of accel, transition to coast
 static const float coast_thresh = 0.5;
-// switch to apogee state after 40 seconds
+
+// Switch to apogee state after 40 seconds
 static const float coast_to_apogee_time_thresh = 40000;
+
 // For preventing AC from actuating even when in coast state
 static const float coast_ac_delay_thresh = 15000;
+
 // Rocket won't leave boost state until burn_timer has exceeded this limit
 static const int burn_time_thresh_ms = 5000;
+
 #endif
