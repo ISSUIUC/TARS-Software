dd895a19d5001756a38faafa01912b38fbc98318
Gautam Dayal <gautam.dayal@yahoo.com>
2022-07-07 13:46:25 -0500

    Added comment to main_rx

--- flight/mcu/src/mcu_telemetry/main_rx.cpp
+++ flight/mcu/src/mcu_telemetry/main_rx.cpp
@@ -1,20 +1,15 @@
-// Arduino9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Arduino9x_TX
-
-
-
-/*
-This code was used to test the RFM LoRa modules on a breadboard:
-    - Frequency: 434Hz
-    - Make sure to change Teensy version in platformio
-    - Make sure to exclude/include the appropriate files to build in platformio.ini
-    - Current problem: Only one-way communication
-*/
+/**
+ * This file contains the code than runs on our ground station
+ * hardware (LoRa Feather module). It includes the receive code, 
+ * command functionality, and interfaces with the ground station GUI
+ * through serial.
+ * 
+ * Spaceshot Telemetry Team 2021-22
+ * Nicholas Phillips
+ * Gautam Dayal
+ * Patrick Marschoun
+ * Peter Giannetos
+ */
 
 #include <SPI.h>
 #include <RH_RF95.h>
