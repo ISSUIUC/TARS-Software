dee6ee1443f6ff0864350068e471bee41f44f678
Rithvik <rbhogavilli@gmail.com>
2022-10-18 18:36:36 -0500

    Inherit RocketFSM in TemplateFSM

--- flight/mcu/lib/teensyShared/TemplateFSM.h
+++ flight/mcu/lib/teensyShared/TemplateFSM.h
@@ -1,35 +1,14 @@
 #ifndef TEMPLATE_FSM_H
 #define TEMPLATE_FSM_H
 
+#include "rocketFSM.h"
 
 struct pointers;
 
-class TemplateFSM {
+class TemplateFSM : public RocketFSM {
    public:
-    enum class FSM_State {
-        STATE_INIT,
-        STATE_IDLE,
-        STATE_LAUNCH_DETECT,
-        STATE_BOOST,
-        STATE_BURNOUT_DETECT,
-        STATE_COAST,
-        STATE_APOGEE_DETECT,
-        STATE_APOGEE,
-        STATE_DROGUE_DETECT,
-        STATE_DROGUE,
-        STATE_MAIN_DETECT,
-        STATE_MAIN,
-        STATE_LANDED_DETECT,
-        STATE_LANDED,
-        STATE_ABORT
-    };
-
     TemplateFSM(pointers *ptr);
-    void tickFSM();
-    FSM_State getFSMState() const { return rocket_state_; }
-
-    private:
-    FSM_State rocket_state_;
+    virtual void tickFSM() override;
 };
 
 #endif
