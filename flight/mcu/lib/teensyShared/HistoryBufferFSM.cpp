f2900bdbe3e83ea5c92ca6e78d62f7eff7f30d07
Aidan Costello <aidancostello@wirelessprv-10-192-134-239.near.illinois.edu>
2022-10-21 09:44:26 -0500

    added new fsm logic to format with multi-fsm system. also updated and added thresholds

--- flight/mcu/lib/teensyShared/HistoryBufferFSM.cpp
+++ flight/mcu/lib/teensyShared/HistoryBufferFSM.cpp
@@ -32,6 +32,23 @@
 #include "pins.h"
 #include "thresholds.h"
 
+#include <cmath>
+#include <map>
+
+std::map<int, String> state_map = { {0, "STATE_INIT"}, {1, "STATE_IDLE"},
+{2, "STATE_LAUNCH_DETECT"},
+{3, "STATE_BOOST"},
+{4, "STATE_BURNOUT_DETECT"},
+{5, "STATE_COAST"},
+{6, "STATE_APOGEE_DETECT"},
+{7, "STATE_APOGEE"},
+{8, "STATE_DROGUE_DETECT"},
+{9, "STATE_DROGUE"},
+{10,"STATE_MAIN_DETECT"},
+{11, "STATE_MAIN"},
+{12, "STATE_LANDED_DETECT"},
+{13, "STATE_LANDED"},
+{14, "STATE_ABORT"}};
 /**
  * @brief Constructor for HistoryBufferFSM class
  * @param pointers
@@ -60,6 +77,8 @@ HistoryBufferFSM::HistoryBufferFSM(pointers *ptr) {
 void HistoryBufferFSM::tickFSM() {
     // Lock mutexes for data used in switch
     chMtxLock(&pointer_struct->dataloggerTHDVarsPointer.dataMutex_highG);
+
+    Serial.println(state_map[(int)rocket_state_]);
 
     // Links to abort for other states
     if (pointer_struct->abort) {
@@ -155,12 +174,118 @@ void HistoryBufferFSM::tickFSM() {
         case FSM_State::STATE_COAST_GNC:
             coast_timer_ = chVTGetSystemTime() - burnout_time_;
 
-            if (TIME_I2MS(coast_timer_) > coast_to_apogee_time_thresh) {
-                rocket_state_ = FSM_State::STATE_APOGEE;
+            if (fabs((*altitude_history_ptr_).getCurrentAverage() - (*altitude_history_ptr_).getPastAverage()) < apogee_altimeter_threshold) {
+                    rocket_state_ = FSM_State::STATE_APOGEE;
+                    apogee_time_ = chVTGetSystemTime();
+                    break;
             }
 
+            if (TIME_I2MS(coast_timer_) < coast_to_apogee_time_thresh) {
+                rocket_state_ = FSM_State::STATE_COAST_GNC;
+            }
+            else {
+                rocket_state_ = FSM_State::STATE_APOGEE;
+                apogee_time_ = chVTGetSystemTime();
+            }
+
+
             break;
+
+
         case FSM_State::STATE_APOGEE:
+            if (fabs((*IMU_acceleration_history_ptr_).getCurrentAverage() - (*IMU_acceleration_history_ptr_).getPastAverage()) > drogue_acceleration_change_threshold_imu) {
+                    rocket_state_= FSM_State::STATE_DROGUE_DETECT;
+                    break;
+            }
+            //potentially add back state to put us back into coast
+
+            
+            if (TIME_I2MS(apogee_timer_) < drogue_deploy_time_since_apogee_threshold) {
+                rocket_state_ = FSM_State::STATE_APOGEE;
+            }
+            else {
+                rocket_state_ = FSM_State::STATE_DROGUE;
+                drogue_time_ = chVTGetSystemTime();
+            }
+            break;
+
+        case FSM_State::STATE_DROGUE_DETECT:
+            if (fabs((*altitude_history_ptr_).getCurrentSecondDerivativeAverage() - (*altitude_history_ptr_).getPastSecondDerivativeAverage()) > drogue_acceleration_change_threshold_altimeter) {
+                    rocket_state_ = FSM_State::STATE_DROGUE;
+                    drogue_time_ = chVTGetSystemTime();
+                    break;
+            }
+
+            else{
+                rocket_state_ = FSM_State::STATE_APOGEE;
+            }
+            break;
+
+        case FSM_State::STATE_DROGUE:
+            drogue_timer_ = chVTGetSystemTime() - drogue_time_;
+            if(TIME_I2MS(drogue_timer_) > refresh_timer){
+                if (fabs((*IMU_acceleration_history_ptr_).getCurrentAverage() - (*IMU_acceleration_history_ptr_).getPastAverage()) > main_acceleration_change_threshold_imu) {
+                        rocket_state_= FSM_State::STATE_MAIN_DETECT;
+                        break;
+                }
+            }
+            
+
+            if (TIME_I2MS(drogue_timer_) < main_deploy_time_since_drogue_threshold) {
+                rocket_state_ = FSM_State::STATE_DROGUE;
+                
+            }
+            else {
+                rocket_state_ = FSM_State::STATE_MAIN;
+                main_time_ = chVTGetSystemTime();
+            }
+            break;
+
+        case FSM_State::STATE_MAIN_DETECT:
+            if (fabs((*altitude_history_ptr_).getCurrentSecondDerivativeAverage() - (*altitude_history_ptr_).getPastSecondDerivativeAverage()) > main_acceleration_change_threshold_altimeter) {
+                    rocket_state_ = FSM_State::STATE_MAIN;
+                    main_time_ = chVTGetSystemTime();
+                    break;
+            }
+            else{
+                rocket_state_ = FSM_State::STATE_DROGUE;
+            }
+            break;
+
+        case FSM_State::STATE_MAIN:
+            main_timer_ = chVTGetSystemTime() - main_time_;
+
+            if(TIME_I2MS(main_timer_) > refresh_timer){
+                if (fabs((*IMU_acceleration_history_ptr_).getCurrentAverage() - (*IMU_acceleration_history_ptr_).getPastAverage()) > landing_imu_threshold) {
+                    rocket_state_ = FSM_State::STATE_LANDED_DETECT;
+                    break;
+                }
+            }
+            
+            if (TIME_I2MS(main_timer_) < main_deploy_time_since_drogue_threshold) {
+                rocket_state_ = FSM_State::STATE_MAIN;
+                
+            }
+            else {
+                rocket_state_ = FSM_State::STATE_LANDED;
+                landing_time_ = chVTGetSystemTime();
+            }
+            break;
+
+        case FSM_State::STATE_LANDED_DETECT:
+            if (fabs((*altitude_history_ptr_).getCurrentAverage() - (*altitude_history_ptr_).getPastAverage())< landing_altimeter_threshold) { // this average will be close to zero
+                    rocket_state_ = FSM_State::STATE_LANDED;
+                    break;
+            }
+
+            else{
+                rocket_state_ = FSM_State::STATE_MAIN;
+            }
+            break;
+
+        case FSM_State::STATE_LANDED:
+            break;
+
 
         default:
             break;
--- flight/mcu/lib/teensyShared/thresholds.h
+++ flight/mcu/lib/teensyShared/thresholds.h
@@ -14,38 +14,48 @@ static const float coast_time_thresh = 300;
 static const float launch_linear_acceleration_thresh = 2;
 
 // Required time of detected acceleration to switch to boost
-static const float launch_time_thresh = 300;
+static const float launch_time_thresh = 20;
 
 // If we observe less than 0.5 g of accel, transition to coast
 static const float coast_thresh = 0.5;
 
-// Switch to apogee state after 26.3 seconds
-static const float coast_to_apogee_time_thresh = 26300;
+// Switch to apogee state after 40 seconds
+static const float coast_to_apogee_time_thresh = 5000;
 
 // For preventing AC from actuating even when in coast state
-static const float coast_ac_delay_thresh = 1000;
+static const float coast_ac_delay_thresh = 15000;
 
 // Rocket won't leave boost state until burn_timer has exceeded this limit
 static const int burn_time_thresh_ms = 5000;
 
 // how small the difference in altimeter data can be before it detects apogee (~ 0, a very small number of some kind)
 // TODO: tune
-static const float apogee_altimeter_threshold = .1;
+static const float apogee_altimeter_threshold = -1;
 
 // see if the change in altitude is close to 0 (or some small number) to detect landing:
 // TODO: tune
+static const float landing_imu_threshold = 0.1;
+
 static const float landing_altimeter_threshold = .1;
 
 // some number greater than 0 to represent a drastic change in acceleration being detected:
 // TODO: tune
-static const float drogue_acceleration_change_threshold_imu = 1;
+static const float drogue_acceleration_change_threshold_imu = 0.1;
+
+static const float drogue_acceleration_change_threshold_altimeter = 0.1;
 
 // some number greater than 0 to represent a drastic change in acceleration being detected:
 // TODO: tune
-static const float main_acceleration_change_ = 1;
+static const float main_acceleration_change_threshold_imu = 0.1;
+
+static const float main_acceleration_change_threshold_altimeter = 0.1;
 
 // this comes from SILSIM (in milliseconds):
 // TODO: tune
-static const float drogue_deploy_time_since_apogee_threshold = 2000;
+static const float drogue_deploy_time_since_apogee_threshold = 10000;
+
+static const float main_deploy_time_since_drogue_threshold = 10000;
+
+static const float refresh_timer = 500;
 
 #endif
