4a4575a05edd526a1bb91c893bf7c46426d82c28
Gautam Dayal <gautam.dayal@yahoo.com>
2022-10-27 15:12:56 -0500

    Added more timer declarations to timer FSM

--- flight/mcu/lib/teensyShared/TimerFSM.h
+++ flight/mcu/lib/teensyShared/TimerFSM.h
@@ -15,4 +15,16 @@ class TimerFSM : public RocketFSM {
     sysinterval_t burn_timer_;
     systime_t burnout_time_;
     sysinterval_t coast_timer_;
+    
+    systime_t apogee_time_;
+    sysinterval_t apogee_timer_;
+
+    systime_t drogue_time_;
+    sysinterval_t drogue_timer_;
+
+    systime_t main_time_;
+    sysinterval_t main_timer_;
+
+    systime_t landing_time_;
+    sysinterval_t landing_timer;
 };
