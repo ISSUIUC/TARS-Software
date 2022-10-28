31035bf6e4b2e8b56d658f7a96348217e4301e3d
colinpk2 <77900054+colinpk2@users.noreply.github.com>
2022-10-28 08:59:32 -0500

    addded flap extension limits

--- flight/mcu/lib/teensyShared/ActiveControl.cpp
+++ flight/mcu/lib/teensyShared/ActiveControl.cpp
@@ -65,6 +65,12 @@ void Controller::ctrlTickFunction(pointers* pointer_struct) {
     u = u + sign * min * dt;
     prev_u = u;
 
+    // Set flap extension limits
+    if (u < min_extension) {
+        u = min_extension;
+    } else if (u > max_extension) {
+        u = max_extension;
+    }
 
     /*
      * When in COAST state, we set the flap extension to whatever the AC
@@ -79,7 +85,7 @@ void Controller::ctrlTickFunction(pointers* pointer_struct) {
         pointer_struct->sensorDataPointer->flap_data.extension = u;
 
     } else {
-        activeControlServos.servoActuation(10);
+        activeControlServos.servoActuation(min_extension);
     }
 }
 
