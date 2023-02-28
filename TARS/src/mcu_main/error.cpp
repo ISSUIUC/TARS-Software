454cc06d1826efed0b96424a0ec8997af7fa7114
sendhil2 <sendhil2@illinois.edu>
2023-02-28 16:53:05 -0600

    error codes fixed

--- TARS/src/mcu_main/error.cpp
+++ TARS/src/mcu_main/error.cpp
@@ -1,3 +1,21 @@
+
+/**
+ * If an error during initialization was detected, some combination of the blue, green, and orange LEDs will be on,
+ * and the red LED will flash at some frequency. The Blue, Green, and Orange LEDs tells you what sensor is errored.
+ *
+ *
+ * GREEN => KX134
+ * GREEN, BLUE => GPS
+ * ORANGE => SD
+ * RED, BLUE, ORANGE => RADIO
+ * BLUE => MAGNETOMETER
+ * BLUE, ORANGE => BNO,
+ * NONE => Something is fucked
+ *
+ * RED, 10 Hz => Connection Error
+ * RED, 2 Hz => Initialization Error
+ */
+
 #include "mcu_main/error.h"
 
 #include <ChRt.h>
@@ -9,42 +27,111 @@ void handleError(ErrorCode code) {
         case NO_ERROR:
             return;
         case CANNOT_CONNECT_KX134_CS:
-            Serial.println("Failed to communicate with KX134. Stalling Program");
-            digitalWrite(LED_RED, HIGH);
-            while (true) { }
-        case CANNOT_INIT_KX134_CS:
-            Serial.println("Could not initialize KX134. Stalling Program");
-            digitalWrite(LED_BLUE, HIGH);
-            while (true) { }
-        case CANNOT_CONNECT_GPS:
-            Serial.println("Failed to communicate with ZOEM8Q0 gps. Stalling Program");
-            while (true) { }
-        case CANNOT_CONNECT_LSM9DS1:
-            Serial.println("Failed to communicate with LSM9DS1 gps. Stalling Program");
-            digitalWrite(LED_ORANGE, HIGH);
-            while (true) { }
-        case SD_BEGIN_FAILED:
-            digitalWrite(LED_RED, HIGH);
-            digitalWrite(LED_ORANGE, HIGH);
-            digitalWrite(LED_BLUE, HIGH);
-            Serial.println("SD Begin Failed. Stalling Program");
+            digitalWrite(LED_GREEN, HIGH);
             while (true) {
+                Serial.println("Failed to communicate with KX134. Stalling Program");
                 digitalWrite(LED_RED, HIGH);
                 delay(100);
                 digitalWrite(LED_RED, LOW);
                 delay(100);
             }
+        case CANNOT_INIT_KX134_CS:
+            digitalWrite(LED_GREEN, HIGH);
+            while (true) {
+                Serial.println("Could not initialize KX134. Stalling Program");
+                digitalWrite(LED_RED, HIGH);
+                delay(500);
+                digitalWrite(LED_RED, LOW);
+                delay(100);
+            }
+        case CANNOT_CONNECT_GPS:
+            digitalWrite(LED_GREEN, HIGH);
+            digitalWrite(LED_BLUE, HIGH);
+            while (true) {
+                Serial.println("Failed to communicate with ZOEM8Q0 gps. Stalling Program");
+                digitalWrite(LED_RED, HIGH);
+                delay(100);
+                digitalWrite(LED_RED, LOW);
+                delay(100);
+            }
+        case CANNOT_CONNECT_LSM9DS1:
+            digitalWrite(LED_GREEN, HIGH);
+            digitalWrite(LED_BLUE, HIGH);
+            while (true) {
+                Serial.println("Failed to communicate with ZOEM8Q0 gps. Stalling Program");
+                digitalWrite(LED_RED, HIGH);
+                delay(500);
+                digitalWrite(LED_RED, LOW);
+                delay(500);
+            }
+        case SD_BEGIN_FAILED:
+            digitalWrite(LED_ORANGE, HIGH);
+            while (true) {
+                Serial.println("SD Begin Failed. Stalling Program");
+                digitalWrite(LED_RED, HIGH);
+                delay(500);
+                digitalWrite(LED_RED, LOW);
+                delay(500);
+            }
         case RADIO_INIT_FAILED:
+            digitalWrite(LED_RED, HIGH);
+            digitalWrite(LED_ORANGE, HIGH);
+            digitalWrite(LED_BLUE, HIGH);
             while (true) {
                 Serial.println("Radio Initialization Failed");
+                digitalWrite(LED_RED, HIGH);
+                delay(100);
+                digitalWrite(LED_RED, LOW);
+                delay(100);
             }
         case RADIO_SET_FREQUENCY_FAILED:
+            digitalWrite(LED_RED, HIGH);
+            digitalWrite(LED_ORANGE, HIGH);
+            digitalWrite(LED_BLUE, HIGH);
             while (true) {
                 Serial.println("[ERROR]: Default setFrequency Failed");
+                digitalWrite(LED_RED, HIGH);
+                delay(500);
+                digitalWrite(LED_RED, LOW);
+                delay(500);
             }
+        case CANNOT_CONNECT_MAGNETOMETER:
+            digitalWrite(LED_BLUE, HIGH);
+            while (true) {
+                Serial.println("[ERROR]: Magnetometer connection failed");
+                digitalWrite(LED_RED, HIGH);
+                delay(100);
+                digitalWrite(LED_RED, LOW);
+                delay(100);
+            }
+        case CANNOT_CONNECT_BNO:
+            digitalWrite(LED_ORANGE, HIGH);
+            digitalWrite(LED_BLUE, HIGH);
+            while (true) {
+                Serial.println("[ERROR]: BNO086 connection failed");
+                digitalWrite(LED_RED, HIGH);
+                delay(500);
+                digitalWrite(LED_RED, LOW);
+                delay(500);
+            }
+        case CANNOT_INIT_BNO:
+            digitalWrite(LED_ORANGE, HIGH);
+            digitalWrite(LED_BLUE, HIGH);
+            while (true) {
+                Serial.println("[ERROR]: BNO086 initialization failed");
+                digitalWrite(LED_RED, HIGH);
+                delay(100);
+                digitalWrite(LED_RED, LOW);
+                delay(100);
+            }
+
         default:
             while (true) {
                 Serial.println("Unknown error");
+                digitalWrite(LED_RED, HIGH);
+                delay(100);
+                digitalWrite(LED_RED, LOW);
+                delay(100);
             }
     }
 
