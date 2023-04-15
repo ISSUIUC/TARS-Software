dfff095dd5350127edd0ea20db380732a7c4daf4
Zerender <davideh3@illinois.edu>
2023-04-15 15:11:17 -0500

    make branch visible

--- TARS/extra_script.py
+++ TARS/extra_script.py
@@ -1,11 +1,13 @@
 # Simple script to add delay between upload and start of unit tests
 # Necessary on some hosts to allow the target Serial port to catch-up
 
-Import("env")
 import time
+Import("env")
+
 
 def after_upload(source, target, env):
-    print ("Delay while uploading...")
+    print("Delay while uploading...")
     time.sleep(1)
+
 
 env.AddPostAction("upload", after_upload)
