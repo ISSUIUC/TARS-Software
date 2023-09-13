15ce96a9847a7d6ab1c4b1aa2313f34678660078
Michael Karpov <mkarpov2@illinois.edu>
2023-09-13 16:03:32 -0500

    Added hilsim run and test script

--- TARS/src/mcu_main/hilsim/flask_app.py
+++ TARS/src/mcu_main/hilsim/flask_app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 import stream_data
 import json
 from flask_cors import CORS, cross_origin
+import os
 
 app = Flask(__name__)
 CORS(app)#, supports_credentials=True)
@@ -11,8 +12,13 @@ CORS(app)#, supports_credentials=True)
 def index():
     if request.method == "POST":
         csvData = json.loads(request.get_data().decode())
-        stream_data.main(csvData)
-    return "Hello"
+        # TODO: validate data here
+        result = ""
+        try:
+            result = stream_data.run_hilsim(csvData['csv_data'])
+        except:
+            return "Internal Server Error", 500
+    return {'log': result}, 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
