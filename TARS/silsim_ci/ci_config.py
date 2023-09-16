ef9705733ed4292c1e5663a0b1565fd05279f511
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-09-16 17:49:24 -0500

    turn repeated function calls into for loops

--- TARS/silsim_ci/ci_config.py
+++ TARS/silsim_ci/ci_config.py
@@ -10,3 +10,6 @@ check_timeouts_length = 10
 # Pass conditions
 check_fsm = True # Check FSM final state
 check_fsm_final_state = 16 # State enum which signifies 'LANDED'
+
+# Data Values
+all_data_vales = ["lowG_data", "highG_data", "gps_data", "barometer_data", "state_data", "voltage_data", "rocketState_data"]
