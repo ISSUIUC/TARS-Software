46b2109b36056aa401ffb42cc955821416d4f7db
Anthony Smykalov <anthony.smykalov@gmail.com>
2023-09-26 23:21:15 -0500

    final minor changes

--- ground/src/feather/main.cpp
+++ ground/src/feather/main.cpp
@@ -503,9 +503,9 @@ void SerialInput(const char* key, const char* value) {
     } else if (strcmp(key, "FLAP") == 0) {
         command.command = CommandType::TEST_FLAP;
     } else if (strcmp(key, "START_LOGGING") == 0) {
-        //command.command = CommandType::START_LOGGING;
+        command.command = CommandType::START_LOGGING;
     } else if (strcmp(key, "STOP_LOGGING") == 0) {
-        //command.command = CommandType::STOP_LOGGING;
+        command.command = CommandType::STOP_LOGGING;
     } else {
         SerialError();
         return;
